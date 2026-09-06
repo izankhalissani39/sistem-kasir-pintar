ec3747d655f0b028e9d8da9c6ceef6014f334cf9
izankhalissani39 <izankhalissani39@gmail.com>
2026-09-07 00:26:23 +0700

    Update database.js

--- database.js
+++ database.js
@@ -49,7 +49,7 @@ const { error } = await supabase.from('transactions').upsert({
   items: transaction.items || [],
 }, { onConflict: 'id' });
 if (error) thow error;
-}
+
 export async function ensureStore(storeName = 'TOKO BERKAH JAYA') {
   if (!supabase) return null;
   const { data, error } = await supabase.rpc('ensure_my_store', { store_name: storeName });
