03efe3185e7620e955773533ca283c54a030cf19
izankhalissani39 <izankhalissani39@gmail.com>
2026-09-06 23:52:19 +0700

    Update database.js

--- database.js
+++ database.js
@@ -47,7 +47,7 @@ const { error } = await supabase.from('transactions').upsert({
   refound_reason: transaction.refoundReason || null,
   cashier_name: transaction.cashierName || '',
   items: transaction.items || [],
-}, { onecnflict: 'id' });
+}, { onConflict: 'id' });
 if (error) thow error;
 }
 export async function ensureStore(storeName = 'TOKO BERKAH JAYA') {
